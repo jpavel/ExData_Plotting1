b8d83a9076eaac979ba4a40af922e9da4e139e26
Pavel Jez <Pavel.Jez@cern.ch>
2015-04-10 17:57:39 +0200

    added interface to read zip file

--- plot1.R
+++ plot1.R
@@ -1,8 +1,19 @@
-plot1<- function(datafile="household_power_consumption.txt"){
+plot1<- function(zipfile="exdata-data-household_power_consumption.zip", datafile="household_power_consumption.txt"){
+      ## UNZIPPING
+      #make tempdir (if not existing)
+      tmpdir<-tempdir()
+      if(!file.exists(tmpdir) dir.create(tmpdir,recursive=TRUE)
+      #unzip to tempdir
+      unzip(zipfile,exdir=tmpdir)
+      inFileName<-paste0(tmpdir,"/",datafile) #full path to unzipped file
+      
+      ## LOADING AND CLEANING DATA
       # load the full file in correct format (separators are ";" and missing values are "?")
       all<-read.table(datafile,header=TRUE, sep=";", na.strings = "?")
       # select only the two days
       data<-all[(all$Date=="1/2/2007" | all$Date=="2/2/2007"),]
+      
+      ## PLOTTING
       # make histogram with requested title, labels and color
       hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
       # copy output to png file with size 480x480 pixels

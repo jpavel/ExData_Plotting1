9683a8a6984b13ae48d471e62cb4409a4f2c7be1
Pavel Jez <Pavel.Jez@cern.ch>
2015-04-10 17:42:05 +0200

    plot1 added

---
+++ plot1.R
@@ -0,0 +1,12 @@
+plot1<- function(datafile="household_power_consumption.txt"){
+      # load the full file in correct format (separators are ";" and missing values are "?")
+      all<-read.table(datafile,header=TRUE, sep=";", na.strings = "?")
+      # select only the two days
+      data<-all[(all$Date=="1/2/2007" | all$Date=="2/2/2007"),]
+      # make histogram with requested title, labels and color
+      hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+      # copy output to png file with size 480x480 pixels
+      dev.copy(png,width=480,height=480,file="plot1.png")
+      #close the file
+      dev.off()
+}
